d04a8529116b6fef46edcfbffbc1878f29d02766
Rihards Baranovskis <multiks2200@gmail.com>
2017-10-01 09:44:59 +0100

    added stochastic mini batches

--- regression.py
+++ regression.py
@@ -22,6 +22,9 @@ import numpy as np
 import pandas as pd
 from random import randint
 import random
+from sklearn.utils import shuffle
+from numpy import random as random_np
+
 
 seed = int(round(random.random()*100000))
 
@@ -59,15 +62,16 @@ sys.stdout = open('./summaries_movies/'+str(seed) + '/log.log', 'w')
         
 dtype_np = np.float64
 
-##shape = 4812, 183
-x = dta_full.drop('worldwide_gross', axis=1).as_matrix().astype(dtype_np)
 ##shape = 4812, 1	
 y = dta_full['worldwide_gross'].as_matrix().astype(dtype_np)
+##shape = 4812, 183
+x = dta_full.drop('worldwide_gross', axis=1).as_matrix().astype(dtype_np)
 
 
 
 
-X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(x, y, test_size=0.3, random_state=1000)
+
+X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(x, y, test_size=0.3, random_state=500)
 
 total_len = X_train.shape[0]
 
@@ -87,17 +91,17 @@ print(Y_test)
 # Parameters
 learning_rate = 0.001
 training_epochs = 1000000
-batch_size = 50
+batch_size = 250
 display_step = 1
-dropout_rate = 0.9
 # Network Parameters
-n_hidden_1 = 90 # 1st layer number of features
-n_hidden_2 = 45 # 2nd layer number of features
-n_hidden_3 = 20
+n_features = X_train.shape[1]
+n_hidden_1 = int(n_features*3) # 1st layer number of features
+n_hidden_2 = int(n_features*1.5) # 2nd layer number of features
+#n_hidden_3 = 20
 #n_hidden_4 = 20
-n_input = X_train.shape[1]
+#n_input = 
 n_classes = 1
-n_features = 183
+
 test_cycle = 100
 drop_rate = 0.2
 
@@ -122,12 +126,12 @@ def multilayer_perceptron(x, params, training):
     layer_2 = tf.nn.relu(layer_2)
     layer_2 = tf.layers.dropout(layer_2, drop_rate, training = training)
 
+    """
     # Hidden layer with RELU activation
     layer_3 = tf.add(tf.matmul(layer_2, params['h3']), params['b3'])
     layer_3 = tf.nn.relu(layer_3)
     layer_3 = tf.layers.dropout(layer_3, drop_rate, training = training)
 
-    """
     # Hidden layer with RELU activation
     layer_4 = tf.add(tf.matmul(layer_3, params['h4']), params['b4'])
     layer_4 = tf.nn.relu(layer_4)
@@ -135,7 +139,7 @@ def multilayer_perceptron(x, params, training):
     """
     
     # Output layer with linear activation
-    out_layer = tf.matmul(layer_3, params['hout']) + params['bout']
+    out_layer = tf.matmul(layer_2, params['hout']) + params['bout']
     return out_layer
 
 
@@ -145,12 +149,12 @@ tf.set_random_seed(seed)
 params = {
     'h1': tf.get_variable("h1", shape=[n_features, n_hidden_1], dtype=dtype_tf, initializer=tf.contrib.layers.variance_scaling_initializer(factor=2.0,  mode='FAN_IN', uniform=False,  seed=None, dtype=dtype_tf)),
     'h2': tf.get_variable("h2", shape=[n_hidden_1, n_hidden_2], dtype=dtype_tf, initializer=tf.contrib.layers.variance_scaling_initializer(factor=2.0,  mode='FAN_IN', uniform=False,  seed=None, dtype=dtype_tf)),
-    'h3': tf.get_variable("h3", shape=[n_hidden_2, n_hidden_3], dtype=dtype_tf, initializer=tf.contrib.layers.variance_scaling_initializer(factor=2.0,  mode='FAN_IN', uniform=False,  seed=None, dtype=dtype_tf)),
+    #'h3': tf.get_variable("h3", shape=[n_hidden_2, n_hidden_3], dtype=dtype_tf, initializer=tf.contrib.layers.variance_scaling_initializer(factor=2.0,  mode='FAN_IN', uniform=False,  seed=None, dtype=dtype_tf)),
     #'h4': tf.get_variable("h4", shape=[n_hidden_3, n_hidden_4], dtype=dtype_tf, initializer=tf.contrib.layers.variance_scaling_initializer(factor=2.0,  mode='FAN_IN', uniform=False,  seed=None, dtype=dtype_tf)),
-    'hout': tf.get_variable("hout", shape=[n_hidden_3, n_classes], dtype=dtype_tf, initializer=tf.contrib.layers.variance_scaling_initializer(factor=2.0,  mode='FAN_IN', uniform=False,  seed=None, dtype=dtype_tf)),
+    'hout': tf.get_variable("hout", shape=[n_hidden_2, n_classes], dtype=dtype_tf, initializer=tf.contrib.layers.variance_scaling_initializer(factor=2.0,  mode='FAN_IN', uniform=False,  seed=None, dtype=dtype_tf)),
     'b1': tf.Variable(tf.random_normal([n_hidden_1], 0, 0.1, dtype=dtype_tf), dtype=dtype_tf),
     'b2': tf.Variable(tf.random_normal([n_hidden_2], 0, 0.1, dtype=dtype_tf), dtype=dtype_tf),
-    'b3': tf.Variable(tf.random_normal([n_hidden_3], 0, 0.1, dtype=dtype_tf), dtype=dtype_tf),
+    #'b3': tf.Variable(tf.random_normal([n_hidden_3], 0, 0.1, dtype=dtype_tf), dtype=dtype_tf),
     #'b4': tf.Variable(tf.random_normal([n_hidden_4], 0, 0.1, dtype=dtype_tf), dtype=dtype_tf),
     'bout': tf.Variable(tf.random_normal([n_classes], 0, 0.1, dtype=dtype_tf), dtype=dtype_tf)
 }
@@ -193,6 +197,7 @@ with tf.Session() as sess:
         avg_cost = 0.
         total_batch = int(total_len/batch_size)
         # Loop over all batches
+        """
         for i in range(total_batch-1):
             batch_x = X_train[i*batch_size:(i+1)*batch_size]
             batch_y = Y_train[i*batch_size:(i+1)*batch_size]
@@ -201,7 +206,19 @@ with tf.Session() as sess:
                                                           ybatch: batch_y, train_pl: True})
             # Compute average loss
             avg_cost += c / total_batch
+        """
+        #do mini batch 
+        # Randomize data point
+        X_train, Y_train = shuffle(X_train, Y_train)
 
+        for i in range(total_batch-1):
+            batch_x = X_train[i*batch_size:(i+1)*batch_size]
+            batch_y = Y_train[i*batch_size:(i+1)*batch_size]
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c, p = sess.run([train_step, cost, ypred], feed_dict={xbatch: batch_x,
+                                                          ybatch: batch_y, train_pl: True})
+            # Compute average loss
+            avg_cost += c / total_batch
 
         # sample prediction
         label_value = np.transpose(batch_y)
@@ -226,7 +243,7 @@ with tf.Session() as sess:
             saver.save(sess, './summaries_movies/' + str(seed) + '/model/',global_step=epoch)
     # Test model
     print ("Optimization Finished!")
-    test_cost, yp_test = sess.run([cost, ypred], feed_dict={xbatch: X_test, ybatch: Y_test, train_pl: True})
+    test_cost, yp_test = sess.run([cost, ypred], feed_dict={xbatch: X_test, ybatch: Y_test, train_pl: False})
     print ("Testing cost=", test_cost)
     print ("r2_score=", r2_score(np.transpose(Y_test), yp_test))
     saver.save(sess, './summaries_movies/' + str(seed) + '/model/',global_step=epoch)
